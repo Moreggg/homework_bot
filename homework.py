d5b593801142e78093c31e68262720b656a952dd
Evgeny Cherednichenko <More1ggg@yandex.ru>
2024-05-28 17:55:26 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -16,16 +16,16 @@ load_dotenv()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-handler = StreamHandler(stream=sys.stdout)
-handler1 = FileHandler(filename=__file__ + '.log', encoding='UTF-8')
+stream_handler = StreamHandler(stream=sys.stdout)
+file_handler = FileHandler(filename=__file__ + '.log', encoding='UTF-8')
 formatter = logging.Formatter(
     ('%(asctime)s [%(levelname)s] [строка %(lineno)d] '
      '[%(funcName)s] %(message)s')
 )
-handler.setFormatter(formatter)
-handler1.setFormatter(formatter)
-logger.addHandler(handler)
-logger.addHandler(handler1)
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -50,18 +50,18 @@ def check_tokens():
         (TELEGRAM_TOKEN, 'TELEGRAM_TOKEN'),
         (TELEGRAM_CHAT_ID, 'TELEGRAM_CHAT_ID')
     )
-    error_message = 'Отсутствует обязательная переменная окружения: '
-    check = True
-    for var in ENV_VARS_KEYS:
-        if var[0] is None:
+    error_message = 'Отсутствуют обязательные переменные окружения: '
+    missed_tokens = []
+    for var, key in ENV_VARS_KEYS:
+        if var is None:
             logger.critical(
-                error_message + var[1]
+                error_message + key
             )
-            check = False
-    if not check:
+            missed_tokens.append(key)
+    if missed_tokens:
         logger.critical('Программа принудительно остановлена')
         raise exceptions.MissedTokensError(
-            error_message + var[1]
+            error_message + ', '.join(missed_tokens)
         )
 
 
@@ -96,11 +96,9 @@ def get_api_answer(timestamp):
                           'headers - {headers}, '
                           'params - {params}, ').format(**api_data)
                          + (f'ошибка: {error}'))
-        logger.error(error_message)
         raise ConnectionError(error_message)
     if homework_statuses.status_code != HTTPStatus.OK:
         error_message = f'Эндпоинт [{ENDPOINT}] недоступен.'
-        logger.error(error_message)
         raise ConnectionError(error_message)
     return homework_statuses.json()
 
@@ -110,16 +108,13 @@ def check_response(response):
     logger.debug(f'Начало проверки ответа от API [{ENDPOINT}]')
     if not isinstance(response, dict):
         error_message = 'Получен не словарь.'
-        logger.error(error_message)
         raise TypeError(error_message)
     if 'homeworks' not in response:
         error_message = 'Отсутствует ключ "homeworks"'
-        logger.error(error_message)
         raise exceptions.EmptyResponseFromAPI(error_message)
     homeworks = response.get('homeworks')
     if not isinstance(homeworks, list):
         error_message = 'Под ключом "homeworks" не список.'
-        logger.error(error_message)
         raise TypeError(error_message)
     return homeworks
 
@@ -129,14 +124,12 @@ def parse_status(homework):
     if 'homework_name' not in homework:
         raise KeyError(f'Отсутствует ключ "homework_name", {homework}')
     status = homework.get('status')
-    if status is None:
+    if not status:
         error_message = 'Отсутствует статус домашней работы'
-        logger.error(error_message)
         raise exceptions.UnknownStatusError(error_message)
     if status not in HOMEWORK_VERDICTS:
         error_message = 'Неизвестный статус домашней работы'
-        logger.error(error_message)
-        raise exceptions.UnknownStatusError(error_message)
+        raise ValueError(error_message)
     homework_name = homework['homework_name']
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -146,10 +139,9 @@ def main():
     """Основная логика работы бота."""
     check_tokens()
     bot = TeleBot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
+    timestamp = 0
 
     old_status = ''
-    old_error = ''
 
     while True:
         try:
@@ -158,21 +150,15 @@ def main():
             if not homeworks:
                 logger.error('Список домашних работ пуст')
                 continue
-            homework = homeworks[0]
-            status = homework['status']
-            if status != old_status:
-                message = parse_status(homework)
-                bot_status = send_message(bot, message)
-            if bot_status:
-                old_status = status
-                timestamp = response.get('current_date')
+            message = parse_status(homeworks[0])
+            if message != old_status and send_message(bot, message):
+                old_status = message
+                timestamp = response.get('current_date', time.time())
         except Exception as error:
             message_error = f'Произошел сбой: {error}'
             logger.error(message_error)
-            if error != old_error:
-                bot_status = send_message(bot, message)
-            if bot_status:
-                old_error = error
+            if error != old_status and send_message(bot, message_error):
+                old_status = error
         finally:
             time.sleep(RETRY_PERIOD)
 
